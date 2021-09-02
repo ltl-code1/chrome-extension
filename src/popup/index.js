b258b97a655184ac807ee75a7a73c4a1e12c2976
tonglei <ltl_code@163.com>
2021-09-02 23:05:09 +0800

    edit

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,11 +1,35 @@
+import { useEffect, useState } from 'react';
 import './popup.scss';
 
 function Popup() {
+
+	const [city, setCity] = useState(null);
+	const [temperature, setTemperature] = useState(null);
+
+
+	useEffect(()=>{
+		window.AMap.plugin('AMap.Weather', function() {
+			//创建天气查询实例
+			var weather = new window.AMap.Weather();
+		
+			//执行实时天气信息查询
+			weather.getLive('上海市', function(err, data) {
+				console.log(err, data);
+				setCity(data.city);
+				setTemperature(data.temperature);
+			});
+		});
+	})
+
 	return (
 		<div className="Popup">
-			Popup
+			<h3 className="title">天气预报</h3>
+			<p>地点：{city}</p>
+			<p>温度：{temperature}</p>
 		</div>
 	);
+
+	
 }
 
 export default Popup;
