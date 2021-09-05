1448edd46c9fdd35d6873d84988cb62aaead7fcc
tonglei <ltl_code@163.com>
2021-09-05 12:29:10 +0800

    edit

--- src/popup/index.js
+++ src/popup/index.js
@@ -2,34 +2,38 @@ import { useEffect, useState } from 'react';
 import './popup.scss';
 
 function Popup() {
-
 	const [city, setCity] = useState(null);
 	const [temperature, setTemperature] = useState(null);
+	const [localCity, setLocalCity] = useState(null);
 
-
+	const getWeather = (city)=> {
+		//创建天气查询实例
+		let weather = new window.AMap.Weather();
+		//执行实时天气信息查询
+		weather.getLive(city, function(err, data) {
+			setTemperature(data.temperature);
+		});
+	}
 	useEffect(()=>{
-		window.AMap.plugin('AMap.Weather', function() {
-			//创建天气查询实例
-			let weather = new window.AMap.Weather();
-			//执行实时天气信息查询
-			weather.getLive('上海市', function(err, data) {
-				setCity(data.city);
-				setTemperature(data.temperature);
-			});
+		//创建定位查询实例对象
+		let citySearch = new window.AMap.CitySearch();
+		//执行定位查询
+		citySearch.getLocalCity((status, result) => {
+			if (status === 'complete' && result.info === 'OK') {
+				setCity(result.city);
+			}
+			getWeather(result.city);
 		});
-
-		window.AMap.plugin('AMap.Geolocation', function() {
-			var geolocation = new window.AMap.Geolocation();
-			geolocation.getCurrentPosition(function(status,result){
-				console.log(result);
-		  });
-		});
-
-	}, [])
+	}, []);
+	useEffect(()=>{
+		getWeather(city);
+	}, [city]);
 
 	return (
 		<div className="Popup">
 			<h3 className="title">天气预报</h3>
+			<input type="text" className="localcity" onChange={e => setLocalCity(e.target.value)}/>
+			<button onClick={() => setCity(localCity)}>提交</button>
 			<p>地点：{city}</p>
 			<p>温度：{temperature}</p>
 		</div>
