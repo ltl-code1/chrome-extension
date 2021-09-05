a6a9401ab28dc4d51640bc1c0ea00cfcbf320b0b
tonglei <ltl_code@163.com>
2021-09-05 17:28:57 +0800

    Squashed commit of the following:
    commit 3d642a9795fb2d9734119919cf90b8523d4dd8c2
    Author: tonglei <ltl_code@163.com>
    Date:   Sun Sep 5 17:28:23 2021 +0800
    
        edit
    
    commit 1448edd46c9fdd35d6873d84988cb62aaead7fcc
    Author: tonglei <ltl_code@163.com>
    Date:   Sun Sep 5 12:29:10 2021 +0800
    
        edit
    
    commit 063f21434aa5d96c7fc6302e3a9d830c3ab135d2
    Author: VIOLAINECAO-PC\Administrator <tongleiliu@soarinfotech.com>
    Date:   Fri Sep 3 18:41:40 2021 +0800
    
        更新 index.js
    
    commit ad53f9972229eb96d2926a722f43adfa313c3765
    Author: VIOLAINECAO-PC\Administrator <tongleiliu@soarinfotech.com>
    Date:   Fri Sep 3 15:56:31 2021 +0800
    
        edit
    
    commit b258b97a655184ac807ee75a7a73c4a1e12c2976
    Author: tonglei <ltl_code@163.com>
    Date:   Thu Sep 2 23:05:09 2021 +0800
    
        edit
    
    commit b891fab51fee9c5254b07d8c176b1870c40810e7
    Author: tonglei <ltl_code@163.com>
    Date:   Wed Sep 1 23:16:05 2021 +0800
    
        edit
    
    commit e6153d4a95b7017c76a2af394d21aa6be7d116ae
    Author: VIOLAINECAO-PC\Administrator <tongleiliu@soarinfotech.com>
    Date:   Wed Sep 1 18:33:46 2021 +0800
    
        edit

--- src/content/index.js
+++ src/content/index.js
@@ -1,1 +1,17 @@
-console.log('content');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+
+
+function Content(){
+    return (
+        <div className="CET-container">
+            <div className="circle"></div>
+        </div>
+    )
+}
+
+const app = document.createElement('div');
+app.id = 'CET-container';
+document.body.appendChild(app);
+ReactDOM.render(<Content />, app);
